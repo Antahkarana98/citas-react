da5b93f257a59f59fb0cc7a7731ade93b94db2dd
Antahkarana98 <Antahkarana98@github.com>
2023-03-26 18:52:52 -0500

    agregando dependencias, tailwind, y algunas cosas a la page'

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,17 @@
+const Formulario = () => {
+  return (
+    <div className="md:w-1/2 lg:2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+      <p className="text-lg mt-5 text-center">
+        Añade pacientes y <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+
+      <form>
+        
+      </form>
+    </div>
+  )
+}
+
+export default Formulario
