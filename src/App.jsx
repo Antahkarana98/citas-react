1d8a79344e8fd07e53c5084eba53228efc83c041
Antahkarana98 <Antahkarana98@github.com>
2023-03-28 17:17:49 -0500

    proyecto finalizado localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const [pacientes, setPacientes] = useState([])
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem("pacientes")) ?? [])
   const [paciente, setPaciente] = useState({})
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes))
+  } , [pacientes])
 
   const eliminarPaciente = id => {
     const pacientesFiltrados = pacientes.filter(paciente => paciente.id !== id)
     setPacientes(pacientesFiltrados)
   }
+
 
   return (
     <div className="container mx-auto mt-20">
